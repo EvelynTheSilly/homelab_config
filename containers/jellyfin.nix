ccb9965101092bfaa106128e821575817a09594a
vlad <vlad@nikulin.name>
2025-04-21 15:06:42 +0100

    minor change

--- containers/jellyfin.nix
+++ containers/jellyfin.nix
@@ -5,7 +5,7 @@
     volumes = [
       "/var/lib/jellyfin/config:/config"
       "/var/cache/jellyfin:/cache"
-      "/home/eve/media:/media"
+      "/home/eve/media/jellyfin:/media"
     ];
     ports = ["8096:8096"]; # Optional if using host networking
     extraOptions = ["--network=host"]; # Important!
