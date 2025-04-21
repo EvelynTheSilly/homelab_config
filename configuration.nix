dc303580e9156508845e067aea0ae46c477200b4
vlad <vlad@nikulin.name>
2025-04-21 03:47:07 +0100

    3 am pain

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
     boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
 
     imports = [
         ./hardware_configuration.nix
