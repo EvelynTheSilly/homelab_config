c0721febe8c27581b067455506cdb4ccc8ea9221
vlad <vlad@nikulin.name>
2025-04-23 20:45:30 +0100

    homepage stuff

--- containers/caddy.nix
+++ containers/caddy.nix
@@ -19,7 +19,7 @@ let Caddyfile = pkgs.writeTextFile{
         }
 
         eve.home {
-            reverse_proxy homepage
+            reverse_proxy homepage:80
             tls internal
         }
     '';
