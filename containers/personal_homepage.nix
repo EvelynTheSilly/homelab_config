45ab9f6972312253b3f65d4649a473b6c675cdc5
vlad <vlad@nikulin.name>
2025-04-23 22:11:16 +0100

    homepage stuff

--- containers/personal_homepage.nix
+++ containers/personal_homepage.nix
@@ -2,6 +2,11 @@
   virtualisation.oci-containers.containers.homepage = {
     image = "registry.eve.home/homepage:latest";
     extraOptions = ["--network=caddy"];
+    environment = {
+        ROCKET_ADDRESS = "0.0.0.0";
+        ROCKET_PORT = "8000";
+    }
+    ;
     autoStart = true;
   };
 }
