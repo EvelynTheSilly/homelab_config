7186bc2a87d065b4559642b34b66734d8941969b
vlad <vlad@nikulin.name>
2025-04-25 17:37:29 +0100

    i wanna die /s

--- containers/tunnel.nix
+++ containers/tunnel.nix
@@ -4,7 +4,7 @@
     environmentFiles = [
         /home/eve/cloudflared/.env
     ];
-    cmd = [ "echo" "$TUNNEL_TOKEN"];#"tunnel" "--no-autoupdate" "run" "--token" "$TUNNEL_TOKEN" "--url" "http://caddy:80" ];
+    cmd = [ "tunnel" "--no-autoupdate" "run" "--token" "\${TUNNEL_TOKEN}" "--url" "http://caddy:80" ];
     extraOptions = ["--network=caddy"];
     autoStart = true;
   };
