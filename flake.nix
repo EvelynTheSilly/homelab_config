f6ff7a83218e728a7205af1761ec99588d0dd8ce
vlad <vlad@nikulin.name>
2025-04-25 01:03:37 +0100

    stuff

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Minimal flake for NixOS config";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-  inputs.unstable-packages.url = "github:NixOS/nixpkgs/unstable";
+  inputs.unstable-packages.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.sops-nix.url = "github:Mic92/sops-nix";
 
   outputs = {
