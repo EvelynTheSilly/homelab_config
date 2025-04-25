571b8eb7a3a15f6f0e3b097f47ba3ec4bd095bfc
vlad <vlad@nikulin.name>
2025-04-25 01:46:09 +0100

    eeeeeeeeeeeee

--- containers/homarr.nix
+++ containers/homarr.nix
@@ -1,4 +1,7 @@
 {lib, ...}:
+let
+    readkey = builtins.readFile /home/eve/homarr-key;
+in
 {
   # imports = ["${unstable-packages}/nixos/modules/virtualisations/oci-containers"];
   virtualisation.oci-containers.containers.homarr = {
@@ -7,7 +10,7 @@
         "7575:7575"
     ];
     environment = {
-        SECRET_ENCRYPTION_KEY = lib.strings.removeSuffix "\n" builtins.readFile /home/eve/homarr-key;
+        SECRET_ENCRYPTION_KEY = lib.strings.removeSuffix "\n" readkey;
     };
     volumes = [
         "/var/run/docker.sock:/var/run/docker.sock" # Optional, only if you want docker integration
