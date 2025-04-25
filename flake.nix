1d29548d8a9700370c1bdc31ed8cb20c63e4b017
vlad <vlad@nikulin.name>
2025-04-25 11:48:36 +0100

    swap to unstable

--- flake.nix
+++ flake.nix
@@ -1,21 +1,16 @@
 {
   description = "Minimal flake for NixOS config";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-  inputs.unstable-packages.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.sops-nix.url = "github:Mic92/sops-nix";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = {
     nixpkgs,
-    unstable-packages,
-    sops-nix,
     ...
   }: {
     nixosConfigurations.homelab = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
-        sops-nix.nixosModules.sops
       ];
     };
   };
