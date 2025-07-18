303423a1d9b010e613e393c53a448285913b2be8
eve <vlad@nikulin.name>
2025-07-18 13:02:36 +0100

    blog stuff

--- containers/personal_homepage.nix
+++ containers/personal_homepage.nix
@@ -1,11 +1,17 @@
 {
   virtualisation.oci-containers.containers.homepage = {
     image = "registry.eve.home/homepage:latest";
-    extraOptions = ["--network=caddy" "--pull=always"];
+    extraOptions = [
+      "--network=caddy"
+      "--pull=always"
+    ];
     environment = {
       ROCKET_ADDRESS = "0.0.0.0";
       ROCKET_PORT = "8000";
     };
+    volumes = [
+      "/home/eve/static:/workspace/homepage/webserver/static"
+    ];
     autoStart = true;
   };
 }
