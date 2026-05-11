3929692f60af802e2f9911ea39501a3c47fc498f
Evelyn <45495711+EvelynTheSilly@users.noreply.github.com.>
2026-05-11 19:13:05 +0100

    i forgor to add :sob:

---
+++ containers/openwebui.nix
@@ -0,0 +1,13 @@
+{
+  virtualisation.oci-containers.containers.openwebui = {
+    image = "ghcr.io/open-webui/open-webui:main";
+    ports = [
+      "3010:8080"
+    ];
+    volumes = [
+      "/home/eve/open_web_ui:/app/backend/data"
+    ];
+    extraOptions = ["--network=caddy"];
+    autoStart = true;
+  };
+}
